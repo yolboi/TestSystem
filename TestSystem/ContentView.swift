28d344f6e37ba707b96b55d97f9fa381aacd08bc
Jarl Roest <s224556@dtu.dk>
2025-02-18 10:25:35 +0100

    Initial Commit

---
+++ TestSystem/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestSystem
+//
+//  Created by Jarl Boyd Roest on 18/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestSystem/TestSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSystemApp.swift
+//  TestSystem
+//
+//  Created by Jarl Boyd Roest on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TestSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
